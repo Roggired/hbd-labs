dd158fcec8208c493553b7d5b76e705d849e3936
ego <aleksei.egoshin@tune-it.ru>
2024-05-01 14:47:29 +0300

    Lab1 and Lab2

---
+++ lab1/postgres_init/init-tables.sql
@@ -0,0 +1,83 @@
+CREATE TABLE dish
+(
+	dish_id BIGSERIAL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	price BIGINT NOT NULL
+);
+
+CREATE TABLE category
+(
+	category_id BIGSERIAL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+	percent INT NOT NULL,
+	min_payment INT NOT NULL
+);
+
+CREATE TABLE client
+(
+	client_id BIGSERIAL PRIMARY KEY,
+	bonus_balance BIGINT NOT NULL,
+	category_id BIGINT REFERENCES category(category_id) ON DELETE CASCADE NOT NULL
+);
+
+CREATE TABLE payment
+(
+	payment_id BIGSERIAL PRIMARY KEY,
+	client_id BIGINT REFERENCES client(client_id) ON DELETE CASCADE NOT NULL,
+	dish_id BIGINT REFERENCES dish(dish_id) ON DELETE CASCADE NOT NULL,
+	dish_amount INT NOT NULL,
+	order_id VARCHAR(255) NOT NULL,
+	order_time TIMESTAMP NOT NULL,
+	order_sum BIGINT NOT NULL,
+	tips BIGINT NOT NULL
+);
+
+CREATE TYPE log_action AS ENUM ('INSERT', 'UPDATE', 'DELETE');
+
+CREATE TABLE logs
+(
+	id BIGSERIAL PRIMARY KEY,
+	table_name VARCHAR(255) NOT NULL,
+	"time" TIMESTAMP NOT NULL,
+	"values" JSONB,
+	action log_action
+);
+
+CREATE FUNCTION log_trigger() RETURNS TRIGGER AS
+$$
+DECLARE
+	new_values JSONB;
+	updated_values JSONB;
+	removed_values JSONB;
+BEGIN
+	IF (TG_OP = 'INSERT') THEN
+		new_values := row_to_json(NEW);
+		INSERT INTO logs(table_name, "time", "values", action) VALUES(TG_TABLE_NAME, now(), new_values, 'INSERT');
+		RETURN NEW;
+	ELSIF (TG_OP = 'UPDATE') THEN
+		updated_values := row_to_json(NEW);
+		INSERT INTO logs(table_name, "time", "values", action) VALUES(TG_TABLE_NAME, now(), updated_values, 'UPDATE');
+		RETURN NEW;
+	ELSIF (TG_OP = 'DELETE') THEN
+		removed_values := row_to_json(OLD);
+		INSERT INTO logs(table_name, "time", "values", action) VALUES(TG_TABLE_NAME, now(), removed_values, 'DELETE');
+		RETURN OLD;
+	END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_dish
+AFTER UPDATE OR INSERT OR DELETE ON dish FOR EACH ROW
+EXECUTE FUNCTION log_trigger();
+
+CREATE TRIGGER check_category
+AFTER UPDATE OR INSERT OR DELETE ON category FOR EACH ROW
+EXECUTE FUNCTION log_trigger();
+
+CREATE TRIGGER check_client
+AFTER UPDATE OR INSERT OR DELETE ON client FOR EACH ROW
+EXECUTE FUNCTION log_trigger();
+
+CREATE TRIGGER check_payment
+AFTER UPDATE OR INSERT OR DELETE ON payment FOR EACH ROW
+EXECUTE FUNCTION log_trigger();
